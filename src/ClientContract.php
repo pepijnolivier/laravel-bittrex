cefeab91aa5ba18cce0a154e6fb789eedbea3de3
Pepijn Olivier <olivier.pepijn@gmail.com>
2017-05-20 02:31:41 +0200

    interface defines the methods to implement

--- src/ClientContract.php
+++ src/ClientContract.php
@@ -3,10 +3,23 @@ namespace Pepijnolivier\Bittrex;
 
 interface ClientContract
 {
-    /**
-     * Get my balances.
-     *
-     * @return array
-     */
     public function getMarkets();
+    public function getCurrencies();
+    public function getTicker($marker);
+    public function getMarketSummaries();
+    public function getMarketSummary($market);
+    public function getOrderBook($market, $type, $depth=20);
+    public function getMarketHistory($market);
+    public function buyLimit($market, $quantity, $rate);
+    public function sellLimit($market, $quantity, $rate);
+    public function cancelOrder($uuid);
+    public function getOpenOrders($market=null);
+    public function getBalances();
+    public function getBalance($currency);
+    public function getDepositAddress($currency);
+    public function withdraw($currency, $quantity, $address, $paymentId=null);
+    public function getOrder($uuid);
+    public function getOrderHistory($market=null);
+    public function getWithdrawalHistory($currency=null);
+    public function getDepositHistory($currency=null);
 }
